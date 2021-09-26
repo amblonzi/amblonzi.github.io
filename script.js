47cf44f4e6a0d7df9d9abc9d5d1eb4b6c2a00de9
amblonzi <54001020+amblonzi@users.noreply.github.com>
2021-09-27 00:30:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "IT Consultant", "Web Developer", "Graphics Designer", ],
+        strings: [ "an IT Consultant", "a Web Developer", "a Graphics Designer", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "IT Consultant", "Web Developer", "Graphics Designer", ],
+        strings: [ "an IT Consultant", "a Web Developer", "a Graphics Designer", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
