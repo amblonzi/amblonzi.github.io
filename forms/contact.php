8ea9a772ad465cb1303714bf2a520a190bfe6cc8
amblonzi <ambvann@gmail.com>
2024-06-08 20:43:39 +0300

    Update on php script

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,8 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'amblonzi.evans@gmail.com';
@@ -12,30 +10,36 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // Check if required POST fields are set
+  if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+    $contact->from_name = $_POST['name'];
+    $contact->from_email = $_POST['email'];
+    $contact->subject = $_POST['subject'];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+    /*
+    $contact->smtp = array(
+      'host' => 'example.com',
+      'username' => 'example',
+      'password' => 'pass',
+      'port' => '587'
+    );
+    */
 
-  echo $contact->send();
+    $contact->add_message( $_POST['name'], 'From');
+    $contact->add_message( $_POST['email'], 'Email');
+    $contact->add_message( $_POST['message'], 'Message', 10);
+
+    echo $contact->send();
+  } else {
+    echo 'Required form fields are missing.';
+  }
 ?>
